ad64f0130bc6f326c09387ee364f5c1bf706dd99
Ian <43066864+IanC13@users.noreply.github.com>
2026-03-09 17:11:48 -0400

    Add dummy test that asserts False
    Added a dummy test that currently fails.

--- polls/tests.py
+++ polls/tests.py
@@ -11,3 +11,6 @@ class PollsSmokeTests(TestCase):
     def test_detail_page_not_found_for_missing_question(self):
         response = self.client.get(reverse("polls:detail", args=(9999,)))
         self.assertEqual(response.status_code, 404)
+
+    def test_dummy(self):
+        self.assertTrue(False)
